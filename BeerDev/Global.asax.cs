d5746303d002883f01f13ba42a88633a7d712bff
psouki <psoukic@gmail.com>
2018-03-10 14:37:23 -0300

    Separation of concerns refactor

--- BeerDev/Global.asax.cs
+++ BeerDev/Global.asax.cs
@@ -11,7 +11,6 @@ namespace BeerDev
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
